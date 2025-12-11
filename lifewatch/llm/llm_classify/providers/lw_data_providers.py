56708a05edc811709f78cd9d74fa7bb50501a389
nikonikoni4 <1553542270@qq.com>
2025-12-11 17:46:21 +0800

    feat:lw_data_providers 增加更新数据操作

--- lifewatch/llm/llm_classify/providers/lw_data_providers.py
+++ lifewatch/llm/llm_classify/providers/lw_data_providers.py
@@ -3,9 +3,12 @@ lifewatch数据提供者
 """
 from lifewatch.storage.database_manager import DatabaseManager
 from lifewatch.config.database import LW_DB_PATH
+
+
+
 class LWDataProviders:
     def __init__(self):
-        self.db_manager = DatabaseManager(LW_DB_PATH,use_pool=True,pool_size=1)
+        self.db_manager = DatabaseManager(LW_DB_PATH,use_pool=True,pool_size=3)
 
     def query_title_description(self,query_list:list[str])->list[dict[str,str]]:
         sql = """
@@ -19,6 +22,15 @@ class LWDataProviders:
             results = cursor.fetchall()
         return [{"key_word": row[0], "description": row[1]} for row in results]
 
+    def update_app_description(self,app_description_list:list[dict[str,str]]):
+        """
+        更新app_description表中的描述
+        """
+        self.db_manager.upsert_many("app_purpose_category",app_description_list,"app")
+
+
+
+lw_data_providers = LWDataProviders() # 全局唯一的实例
 if __name__ == "__main__":
     providers = LWDataProviders()
-    print(providers.query_title_description(["test"]))
+   # providers.update_app_description([{"app":"test","app_description":"test1"},{"app":"test2","app_description":"test2"}])
