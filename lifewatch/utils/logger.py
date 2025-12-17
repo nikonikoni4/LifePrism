80bf0bf08bfd6896b8be4dc7c00ba9e37133a55f
nikonikoni4 <1553542270@qq.com>
2025-12-17 15:41:38 +0800

    feat(utils):添加logger模块

---
+++ lifewatch/utils/logger.py
@@ -0,0 +1,19 @@
+import logging
+# logging全局唯一配置
+logging.basicConfig(
+    level = logging.DEBUG,
+    format = "%(levelname)s func:%(funcName)s line %(lineno)d"
+) # 暂时固定设置，后续增加config内容
+
+def get_logger(name:str,level=None)->logging.Logger:
+    """
+    args:
+        name: 输入logger名称
+        level: 输出等级
+
+    """
+
+    logger = logging.getLogger(name)
+    if level:
+        logger.setLevel(level)
+    return logger
