23a81efb57fcf36acddeeb60c54cffb52651d833
nikonikoni4 <1553542270@qq.com>
2026-02-17 20:20:02 +0800

    增加conflictError

--- lifeprism/utils/__init__.py
+++ lifeprism/utils/__init__.py
@@ -2,12 +2,14 @@
 from .common_utils import is_multipurpose_app
 from .logger import get_logger, setup_file_logging, DEBUG, INFO, WARNING, ERROR
 from .lazy_singleton import LazySingleton
+from .exceptions import ConflictError
 
 __all__ = [
     "get_logger",
     "setup_file_logging",
     "is_multipurpose_app",
     "LazySingleton",
+    "ConflictError",
     "DEBUG",
     "INFO",
     "WARNING",
