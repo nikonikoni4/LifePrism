8653d0588543608be08dfafab8a7ce796d4a3b62
nikonikoni4 <1553542270@qq.com>
2026-04-23 21:21:23 +0800

    feat(export): 导出 Behavior 相关组件

--- frontend/apps/lifewatch/pages/timeline/components/index.ts
+++ frontend/apps/lifewatch/pages/timeline/components/index.ts
@@ -1,6 +1,6 @@
 /**
  * Timeline Components Index
- * 
+ *
  * 导出 Timeline 页面的子组件
  */
 
@@ -13,3 +13,7 @@ export { default as CustomBlockPopover } from './CustomBlockPopover';
 export { CustomBlockAPI } from './customBlockApi';
 // 类型导出
 export * from './types';
+
+// Behavior Summary 相关组件
+export { default as BehaviorBlockLayer } from './BehaviorBlockLayer';
+export { default as BehaviorDetailPanel } from './BehaviorDetailPanel';
