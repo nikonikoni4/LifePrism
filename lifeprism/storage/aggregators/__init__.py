9a5f2db706928e7fd9515e713e51deda56df8aa2
nikonikoni4 <1553542270@qq.com>
2026-04-24 17:48:57 +0800

    feat(storage): 更新 aggregators 导出所有聚合器单例

--- lifeprism/storage/aggregators/__init__.py
+++ lifeprism/storage/aggregators/__init__.py
@@ -3,12 +3,35 @@ Storage Aggregators - 数据聚合层
 
 聚合多个相关 Provider，提供统一的业务数据视图。
 """
-from lifeprism.storage.aggregators.habit_aggregator import habit_aggregator
-from lifeprism.storage.aggregators.mood_aggregator import mood_aggregator
-from lifeprism.storage.aggregators.map_cache_aggregator import map_cache_aggregator
+from lifeprism.storage.aggregators.habit_aggregator import HabitAggregator
+from lifeprism.storage.aggregators.mood_aggregator import MoodAggregator
+from lifeprism.storage.aggregators.goal_aggregator import GoalAggregator
+from lifeprism.storage.aggregators.habit_chain_aggregator import HabitChainAggregator
+from lifeprism.storage.aggregators.category_aggregator import CategoryAggregator
+from lifeprism.storage.aggregators.map_cache_aggregator import MapCacheAggregator
+from lifeprism.utils import LazySingleton
+
+# 创建全局单例
+habit_aggregator = LazySingleton(HabitAggregator)
+mood_aggregator = LazySingleton(MoodAggregator)
+goal_aggregator = LazySingleton(GoalAggregator)
+habit_chain_aggregator = LazySingleton(HabitChainAggregator)
+category_aggregator = LazySingleton(CategoryAggregator)
+map_cache_aggregator = LazySingleton(MapCacheAggregator)
 
 __all__ = [
+    # 类
+    'HabitAggregator',
+    'MoodAggregator',
+    'GoalAggregator',
+    'HabitChainAggregator',
+    'CategoryAggregator',
+    'MapCacheAggregator',
+    # 单例
     'habit_aggregator',
     'mood_aggregator',
+    'goal_aggregator',
+    'habit_chain_aggregator',
+    'category_aggregator',
     'map_cache_aggregator',
 ]
