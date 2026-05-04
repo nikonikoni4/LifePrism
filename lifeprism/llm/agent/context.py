93ede592b7869af85b3226b371da9b75cfbf77bb
nikonikoni4 <1553542270@qq.com>
2026-05-05 00:51:07 +0800

    feat:编排chat context

--- lifeprism/llm/agent/context.py
+++ lifeprism/llm/agent/context.py
@@ -28,25 +28,19 @@ class Context:
 
 
         if type == MessageType.CHAT:
-            # 1. 加载基础文档
-            # 加载agent.md + bootstrap.md + user.md + memory.md
-
-            agent_path    = base + "/agent/chat/agent.md"
-            bootstrap_path = base + "/agent/chat/bootstrap.md"
-            user_path     = base + "/user/user.md"
-            memory_path   = base + "/agent/chat/memory.md"
-
-            for path in [agent_path, bootstrap_path, user_path, memory_path]:
-                content = Context._read_file(path)
-                if content:
-                    parts.append(content)
-            # 2. 加载skill
+            # 1. 系统环境层
+            parts.append(Context._build_identity())
+            # 2. agent定义层和用户定义层
+            parts.append(Context._build_bootstrap())
+            # 3. memory层 短期记忆层
+            parts.append(Context._build_memory())
+            # 4. 加载永远可用的和激活的skill
             skill_loader = SkillLoad()
             skill_load_list = (msg.extra or {}).get("skill_list",None)
-            # parts.append(skill_loader.load_skills(skill_load_list))
-            # 3. 加载可用skill list 
-            # parts.append(skill_loader.load_frontmatters(skill_load_list))
-
+            # 加载特定的skills和allow_skill
+            parts.append(skill_loader.load_skills(skill_load_list))
+            # 5. 加载可用skill list 
+            parts.append(skill_loader.load_frontmatters(skill_load_list))
             return "\n\n".join(parts)
         elif type == MessageType.CLASSIFY:
             classify_preference_path = base + "agent/classify/classify_preference.md"
@@ -63,7 +57,7 @@ class Context:
     @staticmethod
     def build_prompt(system_prompt: str, message: list[dict[str, Any]]):
         return [{"role": "system", "content": system_prompt}] + message
-
+    @staticmethod
     def _build_identity(self)->str:
         """构建系统运行环境提示词"""
         return f"""
@@ -72,13 +66,56 @@ class Context:
         ## workspace
         你当前工作目录是：{str(settings.lifeprism_data_path)}
         """
-
-    def _build_bootstrap(self)->str:
+    @staticmethod
+    def _build_bootstrap()->str:
         """构建引导文档提示词"""
+        # 读取 templates\agent\chat\bootstrap.json 判断是否经过了引导文档处理
+        # 因为不打算提供删除工具，所以只能增加一个文档记录状态
+        bootstrap_json_path = str(settings.lifeprism_data_path) + "/agent/chat/bootstrap.json"
+        content = Context._read_file(bootstrap_json_path)
+        if content:
+            content: dict = json.loads(content)
+            if content.get("bootstrap", False):
+                # 判断bootstrap.md 是否存在
+                bootstrap_path = str(settings.lifeprism_data_path) + "/agent/chat/bootstrap.md"
+                bootstrap_file = Context._read_file(bootstrap_path)
+                if bootstrap_file:
+                    return f"""
+                    ## bootstrap
+                    {bootstrap_file}
+                    """
+        # bootstrap.md 不存在 或 json 不存在 或 json bootstrap 为 False
+        # 添加 soul.md agent.md tool.md user.md
+        soul_path = str(settings.lifeprism_data_path / "agent/chat/soul.md")
+        agent_path = str(settings.lifeprism_data_path / "agent/chat/agent.md")
+        tool_path = str(settings.lifeprism_data_path / "agent/chat/tool.md")
+        user_path = str(settings.lifeprism_data_path / "user/user.md")
+
+        soul_content = Context._read_file(soul_path)
+        agent_content = Context._read_file(agent_path)
+        tool_content = Context._read_file(tool_path)
+        user_content = Context._read_file(user_path)
+        parts = []
+        if soul_content:
+            parts.append(f"## soul\n{soul_content}")
+        if agent_content:
+            parts.append(f"## agent\n{agent_content}")
+        if tool_content:
+            parts.append(f"## tool\n{tool_content}")
+        if user_content:
+            parts.append(f"## user\n{user_content}")
+        return "\n\n".join(parts)
+    @staticmethod
+    def _build_memory()->str:
+        """构建短期记忆层词"""
+        memory_content = Context._read_file(str(settings.lifeprism_data_path / "user/memory.md"))
+        if not memory_content:
+            return ""
         return f"""
-        ## bootstrap
-        你当前引导文档路径：{str(settings.lifeprism_data_path)}/agent/chat/bootstrap.md
+        ## memory
+        {memory_content}
         """
+
 
 
 if __name__ == "__main__":
